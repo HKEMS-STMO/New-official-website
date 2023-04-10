89ce34d929be876a70110fe1b9857eb4fac2c19c
HKEMS-STMO_MQSI <HKEMS-STMO@outlook.com>
2023-04-11 01:01:25 +0800

    同步本地开发

---
+++ js/function.js
@@ -0,0 +1,49 @@
+//ie判断
+function isIE() {
+    if (!!window.ActiveXObject || 'ActiveXObject' in window || navigator.userAgent.indexOf("Edge") > -1) return true
+    else return false
+  }
+
+//获取url上携带的参数
+function getURLData () {
+      var url = window.location.href
+      var theData = new Object()
+      if (url.indexOf('?') != -1) {
+          var _index = -1
+          if (url.indexOf('?') > url.indexOf('#/')) {
+                  _index = url.length
+          } else {
+                  _index = url.indexOf('#/')
+          }
+
+          var str = url.substring(url.indexOf('?') + 1, _index)
+          var strs = str.split('&')
+          for (var i = 0; i < strs.length; i++) {
+          theData[strs[i].split('=')[0]] = strs[i].split('=')[1]
+          }
+      }
+      return theData
+  }
+
+//获取浏览器版本
+function getIEVersion() {
+    var system = {};
+    var ua = navigator.userAgent.toLowerCase();
+    var s;
+    return (s = ua.match(/rv:([\d.]+)\) like gecko/)) ? system.ie = s[1] :
+    (s = ua.match(/msie ([\d.]+)/)) ? system.ie = s[1] :0;
+  }
+
+// 判断ie版本，如果小于11则跳转到下载chrome页
+  var IEVersion = getIEVersion()
+  if (IEVersion && IEVersion < 10) {
+    var params = getURLData()
+    window.location.href = '/function/browser.html'
+  }
+
+//公共部分引用开始
+$(function(){
+    $(".header").load("/function/header.html");
+    $(".footerpage").load("http://www.load.com/footer.html");
+});
+//公共部分引用结束
