f9f14addf19d6246b4556054ae35b2aa0df3e558
HKEMS-STMO_MQSI <HKEMS-STMO@outlook.com>
2023-04-14 19:42:02 +0800

    壮举：添加了网站运行概览系统

---
+++ uptime/config.js
@@ -0,0 +1,21 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '网站运行概况 - 海口实验中学科技社',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm794151211-d728fc7a9ace98c8cb2c73f5',
+    'ur2091008-0279ca138c255ad6498a2fd5',
+  ],
+
+  // 日志天数
+  CountDays: 30,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [],
+};
