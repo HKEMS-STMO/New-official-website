afe578ca4ba9d4d6d21b0abbb9fef99b7e17e6df
HKEMS-STMO_MQSI <HKEMS-STMO@outlook.com>
2023-05-09 22:00:28 +0800

    琐事：UI优化，更换图标库的CDN

--- js/Click on special effects.js
+++ js/Click on special effects.js
@@ -2,7 +2,7 @@ var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
         var a = new Array("厚德","博学","明志","和谐","探索","进取","勤思","创新");
-        var $i = $("<span></span>").text(a[a_idx]);
+        var $i = $("<span id='click'></span>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
         y = e.pageY;
