afdeec66c656719f43c17e8d6fc99559ce3b5f19
山雨欲来风满楼 <maqingshui@outlook.com>
2024-04-05 20:07:12 +0800

    恢复文件，修改域名

---
+++ sitemap/提交脚本.bat
@@ -0,0 +1,5 @@
+curl -H 'Content-Type:text/plain' --data-binary @sitemap.txt "http://data.zz.baidu.com/urls?site=https://hkems-stmo.top&token=4kLILiISB7z60e59"
+
+curl -H 'Content-Type:text/plain' --data-binary @sitemap-file.txt "http://data.zz.baidu.com/urls?site=https://hkems-stmo.top&token=4kLILiISB7z60e59"
+
+pause
